903d89ba46dd8b5f25cffbca4b9ba22e9b8ea593
HenryChen-CH <henry.hao.chen71@gmail.com>
2017-01-31 10:38:17 -0800

    patch httplib to get raw http request

--- integration-test.py
+++ integration-test.py
@@ -1,9 +1,21 @@
 #!/usr/bin/python
 from subprocess import call, Popen
-import requests, os
+import requests, os, httplib
 
 port = 3000
 FNULL = open(os.devnull, 'w')
+
+global http_request_raw
+
+# reference: http://stackoverflow.com/questions/20658572/python-requests-print-entire-http-request-raw
+def patch_send():
+    old_send= httplib.HTTPConnection.send
+    def new_send( self, data ):
+        global http_request_raw
+        http_request_raw = data
+        return old_send(self, data) #return is not necessary, but never hurts, in case the library is changed
+    httplib.HTTPConnection.send= new_send
+patch_send()
 
 server = Popen(['./webserver', 'config'], stdout=FNULL)
 headers = {'content-type': 'text/plain'}
@@ -16,16 +28,15 @@ if r.status_code == requests.codes.ok:
         print "expected content-type is: "+ "text/plain"
         server.kill()
         exit(1);
-
-    # if r.content != r.request.headers:
-    #     print "Test fail"
-    #     print "actual request header: "
-    #     print r.request.headers
-    #     print "response content: "
-    #     print r.content
-    #     server.kill()
-    #     exit(1)
-
+        
+    if r.content != http_request_raw:
+        print "Test fail"
+        print "Request: "
+        print http_request_raw
+        print "Response content: "
+        print r.content
+        server.kill()
+        exit(1)
 else:
     print "status code not 200, test fail"
     server.kill()
