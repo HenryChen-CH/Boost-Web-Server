3c2047353da33c5c6ab6f8dc02dc9589bc2a1cbd
HenryChen-CH <henry.hao.chen71@gmail.com>
2017-02-01 21:43:13 -0800

    use FRIEND_TEST to test private member port of class

--- http_server_test.cpp
+++ http_server_test.cpp
@@ -2,13 +2,17 @@
 #include "http_server.hpp"
 
 using namespace std;
-class http_server_test:public ::testing::Test{
+
+class http_server_test : public ::testing::Test{
 protected:
 	http_server myserver;
-	
-	
 };
+
 TEST_F(http_server_test,porttest){
-	myserver.Init("config");
+	EXPECT_TRUE(myserver.Init("config"));
 	EXPECT_EQ(myserver.port,"3000");
+}
+
+TEST_F(http_server_test, config_file_not_exist){
+	EXPECT_FALSE(myserver.Init("config1"));
 }
