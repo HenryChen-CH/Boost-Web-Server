5695cae2899cf174aefca6efcec3fb93cc51bd63
HenryChen-CH <henry.hao.chen71@gmail.com>
2017-02-01 21:21:00 -0800

    test for request

--- request_test.cpp
+++ request_test.cpp
@@ -1,14 +1,18 @@
 #include "gtest/gtest.h"
 #include "request.hpp"
+#include <string>
 
 using namespace std;
+
 class request_test:public ::testing::Test{
 protected:
 	request myrequest;
-		
 };
-TEST_F(request_test,tostringtest){
-	myrequest.header={'a','b'};
-	myrequest.body={'c','d'};
-	EXPECT_EQ(myrequest.ToString(),"abcd");
+
+TEST_F(request_test,tostringtestgood){
+	string header = "GET / HTTP/1.1\r\n\r\n";
+	string body = "body";
+	copy(header.begin(), header.end(), back_inserter(myrequest.header));
+	copy(body.begin(), body.end(), back_inserter(myrequest.body));
+	EXPECT_EQ(myrequest.ToString(),"GET / HTTP/1.1\r\n\r\nbody");
 }
