9a9eeefad1dbf16f3bb69f469e398efc46544004
HenryChen-CH <henry.hao.chen71@gmail.com>
2017-02-01 21:06:12 -0800

    request parser test

--- request_parser_test.cpp
+++ request_parser_test.cpp
@@ -1,21 +1,33 @@
 #include "gtest/gtest.h"
 #include "request_parser.hpp"
+#include "request.hpp"
+#include <string>
+#include <cstring>
 
 using namespace std;
-class request_parser_test:public ::testing::Test{
+
+class request_parser_test : public ::testing::Test{
 protected:
 	request_parser parser;
-	
-	
+	request req;
+	string http_packet = "GET / HTTP/1.1\r\nContent-Type: text/plain\r\n\r\n";
+	int len = http_packet.length();
+	char *start = strdup(http_packet.c_str());
+	string bad_http_packet = "GET / HTTP/1.1\r\nContent-Type: text/plain\ra\n\r\n";
+	char *bad_start = strdup(bad_http_packet.c_str());
 };
-TEST_F(request_parser_test,parse_test){
-	request myrequest;
-	myrequest.header={} ;   //need editing
-	myrequest.body={} ;  //need editing
-	char* start=new char[1];
-	char* end=new char[1];
-	result_type parse_result=parser.parse(myrequest,start,end);
-	EXPECT_EQ(parse_result, 0);//need editing
+
+TEST_F(request_parser_test, parse_test_good){
+	result_type result = parser.parse(req, start, start+len);
+	EXPECT_EQ(result, good);
 }
 
-//several more cases
+TEST_F(request_parser_test, parse_test_intermedia){
+	result_type result = parser.parse(req, start, start+len-1);
+	EXPECT_EQ(result, intermedia);
+}
+
+TEST_F(request_parser_test, parse_test_bad){
+	result_type result = parser.parse(req, bad_start, bad_start+len+1);
+	EXPECT_EQ(result, bad);
+}
